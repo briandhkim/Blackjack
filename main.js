d2cd7ae5ff0dd9960da0bc6ba5343a5578d748a0
collin_wong <collinwong53@gmail.com>
2017-10-06 16:01:10 -0700

    made init

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+$(document).ready(init);
+var new_deck = null;
+var anthony = null;
+var game =null;
 function CreateDeck(){
     this.deck=[];
     this.create_cards = function(){
@@ -15,17 +19,53 @@ function CreateDeck(){
         var shuffledDeck = []
         while(this.deck.length>0){
             var random_pick = Math.floor(Math.random()*this.deck.length);
-            shuffledDeck.push(this.deck.splice(random_pick,1));
+            shuffledDeck.push(this.deck.splice(random_pick,1)[0]);
         }//end while
         this.deck = shuffledDeck;
     }//end shuffled deck
 }//end object
 
-var test_deck = new CreateDeck();
-
 function Player(){
     this.hand = [];
-    this.score;
+    this.score = 0;
+    this.staying = false;
+    this.get_card = function(card){
+        this.hand.push(card);
+        console.log(this.hand);
+        this.calculator_score();
+    }
+    this.calculator_score = function(){
+        this.score = 0;
+        for(var i = 0; i<this.hand.length; i++){
+            this.score += this.hand[i].value;
+        }
+        console.log(this.score)
+    }
+    this.stay = function(){
+        this.staying  = true;
+    }
+}
 
-
+function BlackJack(){
+    // this.playerTurn = 0;
+    // this.dealer = dealer;
+    this.start_game = function(deck){
+        deck.create_cards();
+        deck.shuffleDeck();
+        console.log(deck.deck);
+    }
+   this.draw_card = function(player,deck){
+        var card  = deck.deck.pop();
+        player.get_card(card);
+   }
+}
+function addClickHandlers(){
+    $('#draw_card').click(game.draw_card(anthony,new_deck.deck))
+}
+function init(){
+    game = new BlackJack();
+    new_deck =  new CreateDeck();
+    game.start_game(new_deck);
+    addClickHandlers();
+    anthony = new Player();
 }
