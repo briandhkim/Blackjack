f165268554c06aa294549e877c444869ba0e4ece
collin_wong <collinwong53@gmail.com>
2017-10-06 18:04:36 -0700

    making changes for multiple players

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ $(document).ready(init);
 var new_deck = null;
 var anthony = null;
 var game =null;
+var player = null;
 function CreateDeck(){
     this.deck=[];
     this.create_cards = function(){
@@ -23,6 +24,11 @@ function CreateDeck(){
         }//end while
         this.deck = shuffledDeck;
     }//end shuffled deck
+    this.make_deck = function(){
+        this.create_cards();
+        this.shuffleDeck();
+        return this.deck;
+    }
 }//end object
 
 function Player(){
@@ -46,18 +52,21 @@ function Player(){
     }
 }
 
-function BlackJack(dealer){
-    this.playerTurn = 0;
-    this.dealer = dealer;
-    this.start_game = function(deck){
-        deck.create_cards();
-        deck.shuffleDeck();
-        console.log(deck.deck);
+function BlackJack(){
+    this.players_array = []
+    this.addplayers = function(number){
+        for(var i = 0; i<number;i++){
+            var player_to_add = new Player();
+            this.players_array.push(player_to_add);
+        }
     }
+    this.playerTurn = 0;
+    this.dealer = new Player();
    this.draw_card = function(player,game){
         var card  = game.deck.pop();
         player.get_card(card);
         this.check_bust(player);
+        // this.compare_score()
         this.playerTurn ++;
 
    }
@@ -67,16 +76,33 @@ function BlackJack(dealer){
            $('#draw_card').hide();
        }
    }
-   this.compare_score =  function(){
-       if(this.player_stay){
-
-       }
+   // this.compare_score =  function(player){
+   //     if(player.stay&&this.dealer.stay){
+   //          if(player.score>this.dealer.score){
+   //              console.log('player wins');
+   //          }
+   //          else{
+   //              console.log('player loses');
+   //          }
+   //     }
+   //  }//end compare sore
+    this.start_game = function(){
+        this.dealer.hand = 0;
+        this.dealer.score = 0;
+        new_deck =  new CreateDeck();
+        var deck = new_deck.make_deck();
+        console.log(deck);
+        game = new BlackJack();
+        new_deck =  new CreateDeck();
+        game.start_game(new_deck);
+        this
+        addClickHandlers();
     }
 }
 function handleDrawClick(){
     game.draw_card(anthony,new_deck)
 }
-function handleStayClick(){
+function handleStayClick(player,player_array){
     anthony.stay = true;
 }
 function addClickHandlers(){
